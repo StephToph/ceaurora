7e567c6241a6635fc3ee17fe347b38b51c9b476c
abiola <omos4real58@gmail.com>
2024-02-17 23:55:13 -0800

    update

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -4,7 +4,7 @@
 ?>
 <!DOCTYPE html>
 <head>
-    <base href="../../../">
+    <base href="<?=site_url(); ?>assets/../">
     <meta charset="utf-8">
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -48,18 +48,6 @@
         }
     </style>
 </head>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/6596ffd10ff6374032bca25d/1hjatej6s';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
 <!--End of Tawk.to Script-->
 <body class="nk-body bg-white npc-general pg-auth">
     <!-- app body @s -->
@@ -68,11 +56,104 @@
             <!-- wrap @s -->
             <div class="nk-wrap nk-wrap-nosidebar">
                 <!-- content @s -->
-                <div class="nk-content " style="background-image: url(<?=site_url('assets/background-color.png'); ?>);
-                    background-size: cover; /* Adjust to 'contain' or other values as needed */
-                    background-position: center;
-                    background-repeat: no-repeat;
-                    margin: 0; /* Remove default body margin */">
+                <div class="nk-content ">
+                    <div class="nk-split nk-split-page nk-split-md">
+                        <div class="nk-split-content nk-block-area nk-block-area-column nk-auth-container bg-white">
+                            <div class="nk-block nk-block-middle nk-auth-body">
+                                <div class="brand-logo pb-5">
+                                    <a href="<?=site_url(); ?>" class="logo-link">
+                                        <img class="logo-light logo-img logo-img-lg" src="<?=site_url(); ?>assets/logo.png" srcset="/demo1/images/logo2x.png 2x" alt="logo">
+                                        <img class="logo-dark logo-img logo-img-lg" src="<?=site_url(); ?>assets/logo.png" srcset="/demo1/images/logo-dark2x.png 2x" alt="logo-dark">
+                                    </a>
+                                </div>
+                                <div class="nk-block-head">
+                                    <div class="nk-block-head-content">
+                                        <h5 class="nk-block-title">Sign-In</h5>
+                                        <div class="nk-block-des">
+                                            <p>Access the DashLite panel using your email and passcode.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <form action="#">
+                                    <div class="form-group">
+                                        <div class="form-label-group"><label class="form-label" for="default-01">Email
+                                                or Username</label><a class="link link-primary link-sm" tabindex="-1"
+                                                href="#">Need Help?</a></div>
+                                        <div class="form-control-wrap"><input type="text"
+                                                class="form-control form-control-lg" id="default-01"
+                                                placeholder="Enter your email address or username"></div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="form-label-group"><label class="form-label"
+                                                for="password">Passcode</label><a class="link link-primary link-sm"
+                                                tabindex="-1" href="auth-reset-v3.html">Forgot Code?</a></div>
+                                        <div class="form-control-wrap"><a tabindex="-1" href="#"
+                                                class="form-icon form-icon-right passcode-switch lg"
+                                                data-target="password"><em
+                                                    class="passcode-icon icon-show icon ni ni-eye"></em><em
+                                                    class="passcode-icon icon-hide icon ni ni-eye-off"></em></a><input
+                                                type="password" class="form-control form-control-lg" id="password"
+                                                placeholder="Enter your passcode"></div>
+                                    </div>
+                                    <div class="form-group"><button class="btn btn-lg btn-primary btn-block">Sign
+                                            in</button></div>
+                                </form>
+                                <div class="form-note-s2 pt-4"> New on our platform? <a
+                                        href="auth-register-v3.html">Create an account</a></div>
+                                <div class="text-center pt-4 pb-3">
+                                    <h6 class="overline-title overline-title-sap"><span>OR</span></h6>
+                                </div>
+                                <ul class="nav justify-center gx-4">
+                                    <li class="nav-item"><a class="link link-primary fw-normal py-2 px-3"
+                                            href="#">Facebook</a></li>
+                                    <li class="nav-item"><a class="link link-primary fw-normal py-2 px-3"
+                                            href="#">Google</a></li>
+                                </ul>
+                                <div class="text-center mt-5"><span class="fw-500">I don't have an account? <a
+                                            href="#">Try 15 days free</a></span></div>
+                            </div>
+                            <div class="nk-block nk-auth-footer">
+                                <div class="nk-block-between">
+                                    <ul class="nav nav-sm">
+                                        <li class="nav-item"><a class="link link-primary fw-normal py-2 px-3"
+                                                href="#">Terms & Condition</a></li>
+                                        <li class="nav-item"><a class="link link-primary fw-normal py-2 px-3"
+                                                href="#">Privacy Policy</a></li>
+                                        <li class="nav-item"><a class="link link-primary fw-normal py-2 px-3"
+                                                href="#">Help</a></li>
+                                        <li class="nav-item dropup"><a
+                                                class="dropdown-toggle dropdown-indicator has-indicator link link-primary fw-normal py-2 px-3"
+                                                data-bs-toggle="dropdown" data-offset="0,10"><small>English</small></a>
+                                            <div class="dropdown-menu dropdown-menu-sm dropdown-menu-end">
+                                                <ul class="language-list">
+                                                    <li><a href="#" class="language-item"><img
+                                                                src="<?=site_url(); ?>assets/images/flags/english.png" alt=""
+                                                                class="language-flag"><span
+                                                                class="language-name">English</span></a></li>
+                                                    <li><a href="#" class="language-item"><img
+                                                                src="<?=site_url(); ?>assets/images/flags/spanish.png" alt=""
+                                                                class="language-flag"><span
+                                                                class="language-name">Español</span></a></li>
+                                                    <li><a href="#" class="language-item"><img
+                                                                src="<?=site_url(); ?>assets/images/flags/french.png" alt=""
+                                                                class="language-flag"><span
+                                                                class="language-name">Français</span></a></li>
+                                                    <li><a href="#" class="language-item"><img
+                                                                src="<?=site_url(); ?>assets/images/flags/turkey.png" alt=""
+                                                                class="language-flag"><span
+                                                                class="language-name">Türkçe</span></a></li>
+                                                </ul>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="mt-3">
+                                    <p>&copy; 2023 DashLite. All Rights Reserved.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="nk-split-content nk-split-stretch bg-abstract"></div>
+                    </div>
                     <div class="brand-logo pb-2 pt-5 text-white text-center">
                         <a href="<?=site_url(); ?>" class="logo-link pt-4">
                             <img class="logo-dark pt-5 " src="<?=site_url(); ?>assets/logo.png" srcset="<?=site_url(); ?>assets/logo.png 2x" alt="logo-dark" height="180">
